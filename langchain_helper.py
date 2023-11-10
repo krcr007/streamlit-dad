28cdce58aaf1d744417abb0fc1e0a4c7c588836c
krcr007 <106908370+krcr007@users.noreply.github.com>
2023-11-10 14:25:13 +0530

    Update langchain_helper.py

--- langchain_helper.py
+++ langchain_helper.py
@@ -4,28 +4,27 @@ from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
-import os
-
+from InstructorEmbedding import INSTRUCTOR
 
 # Create Google Palm LLM model
 llm = GooglePalm(google_api_key="AIzaSyCE8GPxkKGibdVtSpL4SooR_7hS7auBzWI", temperature=0.1)
-# # Initialize instructor embeddings using the Hugging Face model
-from InstructorEmbedding import INSTRUCTOR
+
+# Initialize instructor embeddings using the Hugging Face model
 instructor_embeddings = INSTRUCTOR('hkunlp/instructor-large')
-vectordb_file_path = "faiss_index"
+
+
+csv_file_path = 'encoded-f_a_q_ukl.csv'
 
 def create_vector_db():
     # Load data from FAQ sheet
-    loader = CSVLoader(file_path='encoded-f_a_q_ukl.csv', source_column="prompt")
+    loader = CSVLoader(file_path=csv_file_path, source_column="prompt")
     data = loader.load()
 
     # Create a FAISS instance for vector database from 'data'
-    vectordb = FAISS.from_documents(documents=data,
-                                    embedding=instructor_embeddings)
+    vectordb = FAISS.from_documents(documents=data, embedding=instructor_embeddings)
 
     # Save vector database locally
     vectordb.save_local(vectordb_file_path)
-
 
 def get_qa_chain():
     # Load the vector database from the local folder
@@ -34,6 +33,7 @@ def get_qa_chain():
     # Create a retriever for querying the vector database
     retriever = vectordb.as_retriever(score_threshold=0.7)
 
+    # Define the prompt template
     prompt_template = """Given the following context and a question, generate an answer based on this context only.
     In the answer try to provide as much text as possible from "response" section in the source document context without making much changes.
     If the answer is not found in the context, kindly state "Please contact the HR for the above question" Don't try to make up an answer.
@@ -42,10 +42,10 @@ def get_qa_chain():
 
     QUESTION: {question}"""
 
-    PROMPT = PromptTemplate(
-        template=prompt_template, input_variables=["context", "question"]
-    )
+    # Create a PromptTemplate instance
+    PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
 
+    # Create a RetrievalQA chain
     chain = RetrievalQA.from_chain_type(llm=llm,
                                         chain_type="stuff",
                                         retriever=retriever,
